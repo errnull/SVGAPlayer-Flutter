f31b8cdc619693e14dca3de135180596f3bf6152
errnull <errnull.top@gmail.com>
2019-01-21 12:56:49 +0800

    feat: print hello svga.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:svgaplayer/svgaplayer.dart';
+import 'package:svgaplayer/flutter_svga.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,32 +9,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Svgaplayer.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -45,10 +20,12 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('SVGAPlayer Flutter'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: SVGAPlayer(
+            url: "https://github.com/yyued/SVGA-Samples/blob/master/kingset.svga?raw=true",
+          ),
         ),
       ),
     );
---
+++ lib/src/svgaplayer.dart
@@ -0,0 +1,90 @@
+// import 'dart:async';
+
+// import 'package:flutter/services.dart';
+
+// class Svgaplayer {
+//   static const MethodChannel _channel =
+//       const MethodChannel('svgaplayer');
+
+//   static Future<String> get platformVersion async {
+//     final String version = await _channel.invokeMethod('getPlatformVersion');
+//     return version;
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class SVGAPlayer extends StatefulWidget {
+
+    const SVGAPlayer({
+    Key key,
+    this.url,
+    this.httpHeaders
+  })  : assert(url != null),
+        super(key: key);
+
+  /// The target svga network resource that is displayed.
+  final String url;
+
+  /// Optional headers for the http request of the svga recource url
+  final Map<String, String> httpHeaders;
+
+ @override
+  State<StatefulWidget> createState() => new _SVGAPlayerState();
+}
+
+class _SVGAPlayerState extends State<SVGAPlayer> {
+
+  String showMessage = "Hello SVGA";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text(showMessage);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(SVGAPlayer oldWidget) {
+      super.didUpdateWidget(oldWidget);
+    }
+
+  @override
+  void reassemble() {
+      super.reassemble();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder description) {
+    super.debugFillProperties(description);
+  }
+
+  void _updatePhase() {
+    // TODO: Succcessed.
+    // TODO: Parser SVGA file.
+    setState(() {
+      showMessage = "Load success";
+    });
+  }
+
+  void _fileLoadingFailed() {
+      // TODO: Failed.
+      setState(() {
+      showMessage = "Load failed";
+    });
+  }
+}
